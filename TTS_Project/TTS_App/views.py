cbc184900693289f02e1bb431ca8befcce0fa833
Ajinkya Talekar <91043799+ajinkyatalekar@users.noreply.github.com>
2024-07-18 19:19:14 +0530

    Added login and registration forms

--- TTS_Project/TTS_App/views.py
+++ TTS_Project/TTS_App/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/index.html')
+    args = {}
+    if request.user.is_authenticated:
+        return redirect('logged_in_home')
+    else:
+        return render(request, 'home/index.html', args)
+
+def logged_in_home(request):
+    args = {}
+
+    if request.user.is_authenticated:
+        return render(request, 'home/logged_in_home.html', args)
+    else:
+        return redirect('home')
 
 def about(request):
-    return render(request, 'home/about.html')
+    args = {}
+
+    return render(request, 'home/about.html', args)
